b75ade64a086a66b45c59b5355c81f4dfe3c88cf
Samuel Silva <samuel0100wanderson@gmail.com>
2024-02-27 00:34:16 -0300

    Replace, Strip e Split

--- testes/aula09e.py
+++ testes/aula09e.py
@@ -1,2 +1,20 @@
-#Replace
+#Replace - Trocando caracteres
+print('{:=^20}'.format('REPLACE'))
+
 print('Joao'.replace('a', 'ã'))
+print('Pode ser Pepsi'.replace('Pepsi', 'Água'))
+print('Neymar é Deus do Trovão'.replace('Neymar', 'Thor'))
+
+#Strip - Remover Espaços (Como o =ARRUMAR do Excel)
+print('\n{:=^20}'.format('STRIP'))
+
+texto = '  Samuel  '
+textoStrip = texto.strip()
+print(f'{texto} ------- Tamanho: {len(texto)}')
+print(f'{textoStrip} ------ Tamaho: {len(textoStrip)}')
+
+texto = '====Berg===='
+textoStrip = texto.strip('=')
+
+print(f'\n{texto} -------- Tamanho: {len(texto)}')
+print(f'{textoStrip} --- Tamanho:{len(textoStrip)}')
---
+++ testes/aula09f.py
@@ -0,0 +1,7 @@
+#SPLIT extraindo palavras
+#Converte string em lista com cada palavra sendo um índice
+
+nome = 'Samuel Silva'
+
+nomeStrip = nome.split()
+print(nomeStrip)
