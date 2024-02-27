542ddd841e765b80080e2520d16395a59a45edc6
Samuel Silva <samuel0100wanderson@gmail.com>
2024-02-27 00:38:52 -0300

    JOIN

--- testes/aula09f.py
+++ testes/aula09f.py
@@ -2,6 +2,9 @@
 #Converte string em lista com cada palavra sendo um índice
 
 nome = 'Samuel Silva'
-
 nomeStrip = nome.split()
 print(nomeStrip)
+
+#JOIN unifica as palavras colocando simbolo ou espaço entre
+texto = ', '.join(nomeStrip)
+print(texto)
